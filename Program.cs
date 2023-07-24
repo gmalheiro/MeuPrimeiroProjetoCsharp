d34c4cb4d9c0cb8d529347a37b9ff3a671290196
Gabriel Malheiro <gabrielsilvamalheiro7@gmail.com>
2023-07-23 22:10:06 -0300

    Adicionando ao github

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿// Serve para printar na tela uma mensagem
+//WriteLine escreve uma linha na tela e quebre a linha
+Console.WriteLine("Qual é o seu nome: ");
+string nome = Console.ReadLine()!;
+Console.WriteLine($"Muito Prazer {nome}");
+// Escreva na mesma linha(Permanece na mesma linha)
+Console.Write("Qual é a sua idade:");
+int idade = int.Parse(Console.ReadLine()!);
+Console.WriteLine($"Seu nome é: {nome} e sua idade é: {idade}");
+// Escreva na mesma linha(Permanece na mesma linha)
+
+// System.Out.Println();
